633f4c97831c4439bf076f81ad43a82570bf4168
sanghapal <sanghapalbhowate89@gmail.com>
2014-08-18 12:03:45 -0400

    modified the gem version.

--- lib/sangha_accessibility.rb
+++ lib/sangha_accessibility.rb
@@ -1,15 +1,17 @@
 require 'sangha_accessibility/version'
-require 'action_view'
+require 'action_view/helpers'
 
-module SanghaAccessibility::ActionView::Helpers
+module SanghaAccessibility
   class Base
     include ActionView::Helpers
     include ActionView::Context
     include ActionView::Helpers::CaptureHelper
+    include ActionView::Helpers::TagHelper
 
     extend ActionView::Helpers
     extend ActionView::Context
     extend ActionView::Helpers::CaptureHelper
+    extend ActionView::Helpers::TagHelper
 
     def p_for(value)
       content_tag(:p, value)
