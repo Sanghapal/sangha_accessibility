20d601a6ca047397a7d954e7376b43ee3528214b
sanghapal <sanghapalbhowate89@gmail.com>
2014-08-18 13:39:52 -0400

    modified the gem version.

--- lib/sangha_accessibility.rb
+++ lib/sangha_accessibility.rb
@@ -1,6 +1,6 @@
 require 'sangha_accessibility/version'
 require 'sangha_accessibility/action_view/helpers'
-require 'sangha_accessibility/railtie'
+require 'sangha_accessibility/railtie' if defined?(Rails)
 require 'action_view/helpers'
 
 module SanghaAccessibility
