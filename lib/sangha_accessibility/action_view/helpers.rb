0fccad0cb3ef26dc81e90d57612592f4bf4675a5
sanghapal <sanghapalbhowate89@gmail.com>
2014-08-20 15:13:57 -0400

    modefied the gem version.

--- lib/sangha_accessibility/action_view/helpers.rb
+++ lib/sangha_accessibility/action_view/helpers.rb
@@ -1,13 +1,13 @@
-require 'action_view/helpers'
-require 'action_view/context'
+# require 'action_view/helpers'
+# require 'action_view/context'
 
-module SanghaAccessibility::ActionView::Helpers
+m odule SanghaAccessibility::ActionView::Helpers
   class PGen
     include ActionView::Helpers
     include ActionView::Context
 
-    extend ActionView::Helpers
-    extend ActionView::Context
+#    extend ActionView::Helpers
+#    extend ActionView::Context
 
     def p_for(value)
       content_tag(:p, value)
