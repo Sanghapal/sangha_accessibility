c6d7e50afe1e6c4d6e6367367309ad7d23b8f58d
sanghapal <sanghapalbhowate89@gmail.com>
2014-08-21 17:11:41 -0400

    generate the <p> tag successifully.

--- lib/sangha_accessibility.rb
+++ lib/sangha_accessibility.rb
@@ -2,10 +2,5 @@ require 'sangha_accessibility/version'
 require 'sangha_accessibility/action_view/helpers'
 require 'sangha_accessibility/railtie' if defined?(Rails)
 
-module SanghaAccessibility
-  extend ActiveSupport::Autoload
-  
-  autoload :Helpers
-end
 
 
--- lib/sangha_accessibility/action_view/helpers.rb
+++ lib/sangha_accessibility/action_view/helpers.rb
@@ -1,22 +1,12 @@
-# require 'action_view/helpers'
-# require 'action_view/context'
+require 'action_view/helpers'
+require 'action_view/context'
 
 module SanghaAccessibility
   module ActionView
     module Helpers
-      class MyGemClass 
-      include ActionView::Helpers
-#      include ActionView::Context
-
-      def p_for
-        content_tag :p, "generate the gem"
+      def p_for(value)
+        content_tag(:p, value)
       end
-#      end
-      def new_method_from_gem
-#        "Hello World!"
-        content_tag :p, "generate the p tag"
-      end
-    end
     end
   end
 end
